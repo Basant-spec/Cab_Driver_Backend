c74676c1ef8a74b282efdb7214ae1098a1914e86
Basant <basantdhiman9@gmail.com>
2025-03-11 18:28:23 +0530

    intial done

---
+++ src/main/java/CabDriver/CabDriverBackend/CabDriverBackendApplication.java
@@ -0,0 +1,13 @@
+package CabDriver.CabDriverBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabDriverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabDriverBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CabDriver/CabDriverBackend/CabDriverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package CabDriver.CabDriverBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabDriverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
